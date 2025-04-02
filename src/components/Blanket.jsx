f7fe468d3ddf163a25bf0451ed625e667f82f30d
Aaron Weidele <aweidele@gmail.com>
2025-04-02 11:52:30 -0400

    click

--- src/components/Blanket.jsx
+++ src/components/Blanket.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { pattern, colors, patternColumns } from "../inc/pattern";
 import { Swatch } from "./Swatch";
+const swatches = patternColumns(pattern);
 
 export const Blanket = () => {
   const [swatchColors, setSwatchColors] = useState(colors);
+  const [colorPickerOpen, setColorPickerOpen] = useState(null);
+
+  const handleColorPickerToggle = (swatch) => {
+    console.log(swatch);
+  };
 
   return (
     <div className="aspect-blanket w-full max-w-150">
       <div className="h-full grid grid-cols-(--blanket-cols) grid-rows-(--blanket-rows)">
-        {patternColumns(pattern).map((swatch, i) => (
-          <Swatch key={i} swatch={swatch} color={swatchColors[swatch.color]} />
+        {swatches.map((swatch, i) => (
+          <Swatch key={i} swatch={swatch} color={swatchColors[swatch.color]} handleColorPickerToggle={handleColorPickerToggle} />
         ))}
       </div>
     </div>
--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -1,10 +1,13 @@
-export const Swatch = ({ color, swatch }) => {
+import { useState } from "react";
+
+export const Swatch = ({ color, swatch, handleColorPickerToggle }) => {
   console.log(swatch);
+
   return (
-    <div className="flex relative" style={{ gridColumn: swatch.column, gridRow: `${swatch.row} / ${swatch.row + swatch.span}` }}>
-      <div className="grow -skew-y-25" style={{ backgroundColor: color }}></div>
-      <div className="grow skew-y-25" style={{ backgroundColor: color }}></div>
-      {swatch.color !== "N" && <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">{swatch.color}</div>}
-    </div>
+    <button onClick={() => handleColorPickerToggle(swatch)} className="cursor-pointer flex relative" style={{ gridColumn: swatch.column, gridRow: `${swatch.row} / ${swatch.row + swatch.span}` }}>
+      <div className="transition-all duration-500 grow -skew-y-25" style={{ backgroundColor: color }}></div>
+      <div className="transition-all duration-500 grow skew-y-25" style={{ backgroundColor: color }}></div>
+      {swatch.color !== "N" && <div className="absolute text-sm top-0 left-0 w-full h-full flex items-center justify-center">{swatch.color}</div>}
+    </button>
   );
 };
