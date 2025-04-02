f94b901f3ce56206a15de092bbcf548333738b7f
Aaron Weidele <aweidele@gmail.com>
2025-04-02 11:37:53 -0400

    swatch name

--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -1,9 +1,10 @@
 export const Swatch = ({ color, swatch }) => {
   console.log(swatch);
   return (
-    <div className="flex" style={{ gridColumn: swatch.column, gridRow: `${swatch.row} / ${swatch.row + swatch.span}` }}>
+    <div className="flex relative" style={{ gridColumn: swatch.column, gridRow: `${swatch.row} / ${swatch.row + swatch.span}` }}>
       <div className="grow -skew-y-25" style={{ backgroundColor: color }}></div>
       <div className="grow skew-y-25" style={{ backgroundColor: color }}></div>
+      {swatch.color !== "N" && <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">{swatch.color}</div>}
     </div>
   );
 };
