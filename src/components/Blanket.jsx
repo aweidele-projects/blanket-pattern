013e3d0e8ed617a575e009365347fdeaeb781db2
Aaron Weidele <aweidele@gmail.com>
2025-04-02 12:02:56 -0400

    toggle color picker

--- src/components/Blanket.jsx
+++ src/components/Blanket.jsx
@@ -5,19 +5,23 @@ const swatches = patternColumns(pattern);
 
 export const Blanket = () => {
   const [swatchColors, setSwatchColors] = useState(colors);
-  const [colorPickerOpen, setColorPickerOpen] = useState(null);
+  const [colorPickerOpen, setColorPickerOpen] = useState(false);
+
+  console.log(colorPickerOpen);
 
   const handleColorPickerToggle = (swatch) => {
-    console.log(swatch);
+    setColorPickerOpen((prev) => (prev === swatch ? false : swatch));
   };
 
   return (
-    <div className="aspect-blanket w-full max-w-150">
-      <div className="h-full grid grid-cols-(--blanket-cols) grid-rows-(--blanket-rows)">
-        {swatches.map((swatch, i) => (
-          <Swatch key={i} swatch={swatch} color={swatchColors[swatch.color]} handleColorPickerToggle={handleColorPickerToggle} />
-        ))}
+    <>
+      <div className="aspect-blanket w-full max-w-150">
+        <div className="h-full grid grid-cols-(--blanket-cols) grid-rows-(--blanket-rows)">
+          {swatches.map((swatch, i) => (
+            <Swatch key={i} swatch={swatch} color={swatchColors[swatch.color]} colorPicker={colorPickerOpen === swatch} handleColorPickerToggle={handleColorPickerToggle} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
-export const Swatch = ({ color, swatch, handleColorPickerToggle }) => {
-  console.log(swatch);
+export const Swatch = ({ color, swatch, handleColorPickerToggle, colorPicker }) => {
+  // console.log(swatch);
 
   return (
-    <button onClick={() => handleColorPickerToggle(swatch)} className="cursor-pointer flex relative" style={{ gridColumn: swatch.column, gridRow: `${swatch.row} / ${swatch.row + swatch.span}` }}>
-      <div className="transition-all duration-500 grow -skew-y-25" style={{ backgroundColor: color }}></div>
-      <div className="transition-all duration-500 grow skew-y-25" style={{ backgroundColor: color }}></div>
-      {swatch.color !== "N" && <div className="absolute text-sm top-0 left-0 w-full h-full flex items-center justify-center">{swatch.color}</div>}
-    </button>
+    <div className="relative" style={{ gridColumn: swatch.column, gridRow: `${swatch.row} / ${swatch.row + swatch.span}` }}>
+      <button onClick={() => handleColorPickerToggle(swatch)} className="cursor-pointer flex relative h-full w-full">
+        <div className="transition-all duration-500 grow -skew-y-25" style={{ backgroundColor: color }}></div>
+        <div className="transition-all duration-500 grow skew-y-25" style={{ backgroundColor: color }}></div>
+        {swatch.color !== "N" && (
+          <div className="absolute text-sm top-0 left-0 w-full h-full flex items-center justify-center">
+            {swatch.color}
+            <br />
+            {colorPicker ? "open" : "closed"}
+          </div>
+        )}
+      </button>
+    </div>
   );
 };
