a847a3a6528af4fdd2734b3a9c8fd5a95a2b2360
Aaron Weidele <aweidele@gmail.com>
2025-04-03 09:47:25 -0400

    Yarn swatches

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import { ColorSelector } from "./components/ColorSelector";
 function App() {
   return (
     <>
-      <div className="p-10 flex justify-center items-center h-screen gap-10 max-w-7xl m-auto">
-        <div className="1/3">
+      <div className="p-10 flex items-stretch gap-10 max-w-7xl m-auto max-h-screen">
+        <div>
           <ColorSelector />
         </div>
-        <div className="w-2/3 grow">
-          <Blanket />
+        <div>
+          <p>Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Nunc nec neque. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Fusce egestas elit eget lorem. Nullam quis ante.</p>
+
+          <p>Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Maecenas malesuada. Vestibulum volutpat pretium libero. Etiam vitae tortor. Phasellus dolor.</p>
+
+          <p>Cras id dui. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. Curabitur blandit mollis lacus. Praesent venenatis metus at tortor pulvinar varius. Cras ultricies mi eu turpis hendrerit fringilla.</p>
+
+          <p>Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Pellentesque auctor neque nec urna. Morbi nec metus. Curabitur vestibulum aliquam leo. Nulla consequat massa quis enim.</p>
+
+          <p>Maecenas malesuada. Phasellus accumsan cursus velit. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Vivamus consectetuer hendrerit lacus. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi.</p>
         </div>
+
+        {/* <div>
+          <Blanket />
+        </div> */}
       </div>
     </>
   );
--- src/components/ColorSelector.jsx
+++ src/components/ColorSelector.jsx
@@ -3,10 +3,12 @@ import { YarnColor } from "./YarnColor";
 
 export const ColorSelector = () => {
   return (
-    <div className="grid grid-cols-[repeat(6,3.5rem)] gap-1">
-      {products.map((product) => (
-        <YarnColor key={product.name} yarn={product} />
-      ))}
+    <div className="h-full overflow-auto p-5">
+      <div className="grid grid-cols-5 gap-5">
+        {products.map((product) => (
+          <YarnColor key={product.name} yarn={product} />
+        ))}
+      </div>
     </div>
   );
 };
