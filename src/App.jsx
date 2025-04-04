77720c202e1ecec07b241235592e67ad60d3fe15
Aaron Weidele <aweidele@gmail.com>
2025-04-04 13:00:46 -0400

    Adding my website to footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Blanket } from "./components/Blanket";
 import { BlanketProvider } from "./components/BlanketContext";
 import { ColorSelector } from "./components/ColorSelector";
-import { GithubLogo } from "./components/GithubLogo";
+import { AWComLogo, GithubLogo } from "./components/Logos";
 function App() {
   return (
     <>
@@ -15,12 +15,17 @@ function App() {
           </div>
         </BlanketProvider>
       </div>
-      <div className="fixed left-5 bottom-5">
+      <footer className="fixed left-5 bottom-5 flex items-center gap-4 text-white">
         <a className="flex gap-4 items-center text-white hover:text-canary text-xs" href="https://github.com/aweidele-projects/blanket-pattern" target="_blank">
           <GithubLogo className="fill-current h-auto block w-6" />
           <span>View this project in Github</span>
         </a>
-      </div>
+        <span>|</span>
+        <a className="flex gap-4 items-center text-white hover:text-canary text-xs" href="https://www.aaronweidele.com/" target="_blank">
+          <AWComLogo className="fill-current h-auto block w-6" />
+          <span>aaronweidele.com</span>
+        </a>
+      </footer>
     </>
   );
 }
