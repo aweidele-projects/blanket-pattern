07e235b286114d0f38f1b4ef885630cea5475553
Aaron Weidele <aweidele@gmail.com>
2025-04-03 22:25:22 -0400

    Interface improvements

--- src/components/CurrenColors.jsx
+++ src/components/CurrenColors.jsx
@@ -3,7 +3,7 @@ import { BlanketContext } from "./BlanketContext";
 import products from "../inc/products.json";
 
 export const CurrentColors = () => {
-  const { patternColors } = useContext(BlanketContext);
+  const { patternColors, handleToggleColor } = useContext(BlanketContext);
 
   const currentColors = Object.fromEntries(Object.entries(patternColors).map(([key, productId]) => [key, products.find((product) => product.id === productId) || null]));
 
@@ -12,14 +12,16 @@ export const CurrentColors = () => {
       <h2 className="text-2xl font-bold mb-4">Current Colors</h2>
       <ul className="grid grid-cols-2 gap-3">
         {Object.entries(currentColors).map(([key, yarn]) => (
-          <li key={key} className="flex gap-3 items-center mb-4 border border-zinc-500 p-4">
-            <h3 className="text-xl font-bold text-center">{key}</h3>
-            <div className="flex flex-col items-center gap-2 w-full">
-              <div className="w-20 h-20 rounded-full overflow-hidden">
-                <img src={`/images/${yarn.image}`} />
+          <li key={key}>
+            <button onClick={() => handleToggleColor(key)} className="w-full flex gap-3 items-center mb-4 border border-zinc-500 p-4">
+              <h3 className="text-xl font-bold text-center">{key}</h3>
+              <div className="flex flex-col items-center gap-2 w-full">
+                <div className="w-20 h-20 rounded-full overflow-hidden">
+                  <img src={`/images/${yarn.image}`} />
+                </div>
+                <div className="text-sm uppercase font-light">{yarn.name}</div>
               </div>
-              <div className="text-sm uppercase font-light">{yarn.name}</div>
-            </div>
+            </button>
           </li>
         ))}
       </ul>
--- src/components/YarnColor.jsx
+++ src/components/YarnColor.jsx
@@ -3,14 +3,15 @@ import { BlanketContext } from "./BlanketContext";
 import { getTextColor } from "../inc/pattern";
 
 export const YarnColor = ({ yarn }) => {
+  console.log("YarnColor()");
   const { patternColors, selectedColor, handleSetPatternColor } = useContext(BlanketContext);
 
   const selectedColorId = patternColors[selectedColor];
-  console.log(patternColors, selectedColor, selectedColorId);
+  console.log(yarn, selectedColorId);
   return (
     <>
       <button onClick={() => handleSetPatternColor(selectedColor, yarn.id)} className="text-[10px] text-white uppercase flex flex-col justify-start gap-2 cursor-pointer">
-        <div className={`rounded-full overflow-hidden`}>
+        <div className={`rounded-full overflow-hidden${selectedColorId === yarn.id ? " border-2 shadow-(--glow)" : ""}`}>
           <img src={`/images/${yarn.image}`} />
         </div>
         <div className="grow p-1 flex justify-center items-center" style={{ backgroundColor: yarn.color }}>
